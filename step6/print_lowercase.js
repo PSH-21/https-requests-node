858dcea42f6015ba5ed5af2042a171e42097af05
Peter Hunt <peter.s.hunt@gmail.com>
2017-09-11 20:22:10 +0000

    uppercase finished

---
+++ step6/print_lowercase.js
@@ -0,0 +1,15 @@
+var getHTML = require('../step5').getHTML,
+    printHTML = require('../step5').printHTML
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printLowerCase (html) {
+
+
+
+}
+
+getHTML(requestOptions, printUpperCase);
